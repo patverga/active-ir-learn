1aa076455c848895a589db52a20d19736b2850bc
Patrick Verga <pat@sydney.cs.umass.edu>
2015-01-21 17:03:51 -0500

    print statemtns

--- src/main/python/gensim_things.py
+++ src/main/python/gensim_things.py
@@ -5,7 +5,9 @@ import gensim.models as gm
 import logging
 
 sentences = LineSentence('out.docs')
+print "Making Bigrams"
 bigrams = gm.Phrases(sentences)
+print "Making Trigrams"
 trigrams = gm.Phrases(bigrams[sentences])
 
 # for sentence in trigrams[sentences]:
@@ -14,6 +16,7 @@ trigrams = gm.Phrases(bigrams[sentences])
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
+print "Training model"
 model = gm.Word2Vec(trigrams[sentences], workers=8, size=500, window=10, negative=10, min_count=1)
 # model.train(bigrams[sentences])
 # model.train(sentences)
